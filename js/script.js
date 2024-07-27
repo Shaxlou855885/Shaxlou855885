b7f53c5b8606307494195d160c2c46eec5b7b0d5
Joe <javokhir.369@gmail.com>
2024-07-27 21:13:49 +0500

    change portfolio projects

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'YouTuber', 'Blogger',],
+    strings: ['Frontend-разработчик ', 'Ютубер', 'Блоггер',],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
